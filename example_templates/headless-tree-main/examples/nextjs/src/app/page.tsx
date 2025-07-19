ec57820e450abfe60abebdc77c28d7b0d87d49a0
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2025-07-18 22:20:06 -0500

    chore: exclude example_templates folder from repository
    - Added example_templates to .gitignore
    - Removed all example template files from git tracking to reduce repository size

--- example_templates/headless-tree-main/examples/nextjs/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Tree } from "@/app/tree";
-
-export default function Home() {
-  return (
-    <div>
-      <Tree />
-    </div>
-  );
-}
--- example_templates/headless-tree-main/packages/docs/src/util/use-story.ts
+++
@@ -1,5 +0,0 @@
-import { useStories } from "@site/src/util/use-stories";
-import { useMemo } from "react";
-
-export const useStory = (storyId: string) =>
-  useStories(useMemo(() => [storyId], [storyId]))?.[0] ?? null;
